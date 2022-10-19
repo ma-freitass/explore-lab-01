803c63d5e0d6ee9f46c747436d3f6f268ea46bc9
Maiala Freitas <maiala_rodrigues@hotmail.com>
2022-10-19 16:37:53 -0300

    git committed - m "added imask"

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 import "./css/index.css"
+import IMask from "imask"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#4360099", "#2D57F2"],
+    mastercard: ["#C69347", "#DF6F29"],
+    default: ["pink", "purple"],
+  }
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
+
+//security code
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
